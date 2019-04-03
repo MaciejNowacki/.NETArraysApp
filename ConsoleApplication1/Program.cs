f0a9bcd8e0a673b41189bac14583ba48d3e9f33b
MaciejNowacki <maciejnowacki@protonmail.com>
2019-04-03 11:55:20 +0200

    Final version

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static int[] num = new int[1];
+        private static int index = 0;
+        
+        static void Main(string[] args)
+        {      
+            Console.WriteLine(num.Length);
+            printArray();
+            add(1);
+            Console.WriteLine(num.Length);
+            printArray();
+            add(2);
+            Console.WriteLine(num.Length);
+            printArray();
+            add(3);
+            Console.WriteLine(num.Length);
+            printArray();
+            Console.WriteLine(getElement(2));
+            setElement(2, 4);
+            Console.WriteLine(num.Length);
+            printArray();
+            setElement(10, 4);
+            Console.WriteLine(num.Length);
+            printArray();
+            add(3);
+            Console.WriteLine(num.Length);
+            printArray();
+        }
+
+        static void add(int value)
+        {
+            if(index < num.Length)
+            {
+                num[index++] = value;
+            }
+            else
+            {
+                resizeArrayTo(2*num.Length);
+                num[index++] = value;
+            }
+        }
+
+        static void resizeArrayTo(int newSize)
+        {
+            int[] tmp = new int[num.Length];
+            Array.Copy(num, tmp, index);
+            num = new int[newSize];
+            Array.Copy(tmp, num, index);
+        }
+
+
+        static int getElement(int i)
+        {
+            if (i >= index) //moze num.Length? Czy mamy pobierac obszar zarezerwowany wartosciami domyslnymi, czy jedynie ten nadpisany przez nas?
+            {
+                throw new System.ArgumentException("Index poza zakresem!");
+            }
+            return num[i];
+        }
+
+        static void setElement(int i, int value)
+        {
+            resizeArrayTo(i+1);
+            num[i] = value;
+            index = i + 1;
+        }
+
+        static void printArray()
+        {
+            for(int i = 0; i < index; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.WriteLine("------------------");
+        }
+    }
+}
