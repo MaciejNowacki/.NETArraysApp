d4728b278de8e4aaee4d21a633b2206bef40465a
Nowacki.Maciej <209408@edu.p.lodz.pl>
2019-05-22 13:36:19 +0200

    Abstracted to class ArrayClass adn fix delegates

---
+++ ConsoleApplication1/Array.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public delegate void MyDelegate(int sizeOfArray);
+    public class ArrayClass
+    {
+        private MyDelegate m_oMyDelegate = null;
+        private int[] num = new int[1];
+        private int index = 0;
+
+        public ArrayClass()
+        {
+            m_oMyDelegate += displaySizeOfArray;
+        }
+
+        public void displaySizeOfArray(int sizeOfArray)
+        {
+            Console.WriteLine("Rozmiar tablicy: " + sizeOfArray);
+        }
+
+        public void add(int value)
+        {
+            if (index < num.Length)
+            {
+                num[index++] = value;
+            }
+            else
+            {
+                resizeArrayTo(2 * num.Length);
+                num[index++] = value;
+            }
+            m_oMyDelegate(num.Length);
+        }
+
+        public void resizeArrayTo(int newSize)
+        {
+            int[] tmp = new int[num.Length];
+            Array.Copy(num, tmp, index);
+            num = new int[newSize];
+            Array.Copy(tmp, num, index);
+            m_oMyDelegate(num.Length);
+        }
+
+
+        public int getElement(int i)
+        {
+            if (i >= index) //moze num.Length? Czy mamy pobierac obszar zarezerwowany wartosciami domyslnymi, czy jedynie ten nadpisany przez nas?
+            {
+                throw new System.ArgumentException("Index poza zakresem!");
+            }
+            return num[i];
+        }
+
+        public void setElement(int i, int value)
+        {
+            if (num.Length < i)
+            {
+                resizeArrayTo(i + 1);
+            }
+            num[i] = value;
+            index = i + 1;
+        }
+
+        public void printArray()
+        {
+            Console.WriteLine("-------- [START] -------");
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.WriteLine("--------- [STOP] -------");
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -5,94 +5,26 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
-    public delegate void MyDelegate(int sizeOfArray);
     class Program
     {
-        private static MyDelegate m_oMyDelegate = null;
-        private static int[] num = new int[1];
-        private static int index = 0;
-        
         static void Main(string[] args)
         {
-            m_oMyDelegate += displaySizeOfArray;
-            m_oMyDelegate(num.Length);
-            printArray();
-            add(1);
-            m_oMyDelegate(num.Length);
-            printArray();
-            add(2);
-            m_oMyDelegate(num.Length);
-            printArray();
-            add(3);
-            m_oMyDelegate(num.Length);
-            printArray();
-            Console.WriteLine(getElement(2));
-            setElement(2, 4);
-            m_oMyDelegate(num.Length);
-            printArray();
-            setElement(10, 4);
-            m_oMyDelegate(num.Length);
-            printArray();
-            add(3);
-            m_oMyDelegate(num.Length);
-            printArray();
+            ArrayClass array = new ArrayClass();
+            array.printArray();
+            array.add(1);
+            array.printArray();
+            array.add(2);
+            array.printArray();
+            array.add(3);
+            array.printArray();
+            Console.WriteLine(array.getElement(2));
+            array.setElement(2, 4);
+            array.printArray();
+            array.setElement(10, 4);
+            array.printArray();
+            array.add(3);
+            array.printArray();
             Console.ReadLine();
-        }
-
-        static void displaySizeOfArray(int sizeOfArray)
-        {
-            Console.WriteLine("Rozmiar tablicy: " + sizeOfArray);
-        }
-
-        static void add(int value)
-        {
-            if(index < num.Length)
-            {
-                num[index++] = value;
-            }
-            else
-            {
-                resizeArrayTo(2*num.Length);
-                num[index++] = value;
-            }
-        }
-
-        static void resizeArrayTo(int newSize)
-        {
-            int[] tmp = new int[num.Length];
-            Array.Copy(num, tmp, index);
-            num = new int[newSize];
-            Array.Copy(tmp, num, index);
-        }
-
-
-        static int getElement(int i)
-        {
-            if (i >= index) //moze num.Length? Czy mamy pobierac obszar zarezerwowany wartosciami domyslnymi, czy jedynie ten nadpisany przez nas?
-            {
-                throw new System.ArgumentException("Index poza zakresem!");
-            }
-            return num[i];
-        }
-
-        static void setElement(int i, int value)
-        {
-            if(num.Length < i)
-            {
-                resizeArrayTo(i + 1);
-            }
-            num[i] = value;
-            index = i + 1;
-        }
-
-        static void printArray()
-        {
-            Console.WriteLine("-------- [START] -------");
-            for (int i = 0; i < index; i++)
-            {
-                Console.WriteLine(num[i]);
-            }
-            Console.WriteLine("--------- [STOP] -------");
         }
     }
 }
